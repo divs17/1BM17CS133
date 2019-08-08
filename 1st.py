9bdbeae4a84ba72f15aa63a6b50ad6f64f13f4f6
divs17 <53867053+divs17@users.noreply.github.com>
2019-08-08 12:39:36 +0530

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,80 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5+5
+10
+>>> 5-7
+-2
+>>> 5*5
+25
+>>> 5/5
+1.0
+>>> 5>6
+False
+>>> 5<6
+True
+>>> 48>456
+False
+>>> print('HELLO WORLD')
+HELLO WORLD
+>>> 60>45 or 45<46
+True
+>>> if True:
+   print ("True")
+
+else:
+   print ("False")
+
+   
+True
+>>> if true:
+	print("ktha malai")
+
+	
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    if true:
+NameError: name 'true' is not defined
+>>> if true:
+	print("ktha malai")
+else:
+	print("malai taha cha")
+
+	
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    if true:
+NameError: name 'true' is not defined
+>>> if True:
+   print ("True")
+
+else:
+   print ("False")
+
+True
+>>> counter = 100          
+miles   = 1000.0      
+name    = "John"  
+print (counter)
+print (miles)
+print (name)
+SyntaxError: multiple statements found while compiling a single statement
+>>> counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print (counter)
+print (miles)
+print (name)
+SyntaxError: multiple statements found while compiling a single statement
+>>>  counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print (counter,'python')
+print (miles,'python')
+print (name,'python')
+SyntaxError: unexpected indent
+>>> 
+=================== RESTART: C:/Users/bmsce/Desktop/add.py ===================
+30
+>>> 
