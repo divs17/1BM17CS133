41e80fb7122d99b60b4b35ada4caedc6e7f9c517
divs17 <53867053+divs17@users.noreply.github.com>
2019-10-17 12:20:05 +0530

    Add files via upload

--- happynumbers.py
+++ happynumbers.py
@@ -1,41 +1,26 @@
-f=open("1.txt","w")
-for num in range(2,1000):
-    if all(num%i!=0 for i in range(2,num)):
-       f.write(str(num)+' ')
-f.close()
+prime = open('happy-no.txt', 'r')
+happy = open('prime_no.txt', 'r')
 
-f1=open("2.txt","w")
-def isHappyNumber(num):    
-    rem = sum = 0;    
-    while(num > 0):    
-        rem = num%10;    
-        sum = sum + (rem*rem);    
-        num = num//10;    
-    return sum;    
-            
-   
-for i in range(1, 1001):    
-    result = i;    
-    while(result != 1 and result != 4):    
-        result = isHappyNumber(result);    
-        
-    if(result == 1):    
-        f1.write(str(i)+' ')    
+prime_string = prime.read()
+happy_string = happy.read()
 
-f1.close()
-print("\nPRIME NUMBERS BETWEEN 1-1000 \n")
-f=open("1.txt","r")
-primes=f.read()
-li_prime=primes.split(' ')
-print(li_prime)
+prime_no = prime_string.split(",")
+happy_no = happy_string.split(",")
 
-print("\nHAPPY NUMBERS BETWEEN 1-1000 \n")
-f1=open("2.txt","r")
-happy=f1.read()
-li_happy=happy.split(' ')
-print(li_happy)
+prime_no[-1] = prime_no[-1].strip()
+happy_no[-1] = happy_no[-1].strip()
 
-print("\n\nCOMMON NUMBERS:")
-for i in li_prime:
-    if i in li_happy:
-        print(i,end=",")
+overlap = []
+
+for number in prime_no:
+     if number in happy_no:
+          overlap.append(number)
+
+print("The overlapping numbers are: ",overlap)
+
+'''
+Output:
+The overlapping numbers are:  
+[' 7', ' 13', ' 19', ' 23', ' 31', ' 79', ' 97', ' 103', ' 109', ' 139', ' 167', ' 193', ' 239', ' 263', ' 293', ' 313',
+' 331', ' 367', ' 379', ' 383', ' 397', ' 409', ' 487', ' 563', ' 617', ' 653', ' 673', ' 683', ' 709', ' 739', ' 761',
+' 863', ' 881', ' 907', ' 937']
